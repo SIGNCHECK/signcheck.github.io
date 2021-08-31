c3c3589f5e587e2bc40f50eb740608c4613e8411
zoom <1183408945@qq.com>
2021-08-31 20:57:15 +0800

    version 0.9

---
+++ static/works/DYT_HOTEL_QINGDAO/config.js
@@ -0,0 +1,59 @@
+module.exports={
+  id: "Diaoyutai Hotel",
+  works_content_en: {
+    cover_title: "BASIS INTERNATIONAL SCHOOL, HUIZHOU",
+    tag_list: ["School"],
+    title: "BASIS INTERNATIONAL SCHOOL",
+    location:"HUIZHOU",
+    client: "China Resources",
+    year: "2018",
+    intro: "Unique international campus style\n\nBASIS Curriculum Schools are consistently top rated in the United States. Co-founded by economists Dr. Michael Block and Olga Block in 1998. BASIS International School Park Lane Harbor (BIPH) covers 68,744 square meters with a total building area of 43,356 squaremeters, making the floor area ratio of only 0.59, and only 22% of the building coverage. It is a rare ecological campus, which is built on mountains with green space of up to 65%..\n" +
+      "\n" +
+      "The design fully takes into account the needs of users. The physiological needs of the student population, such as height, visual visibility, text level, and behavioral and psychological characteristics, are incorporated into the service scope of the system, and integrated into the specific design in accordance with the environment and cultural learning atmosphere. In a complex environment, it effectively plays the role of direction and warning, so that the audience can accurately understand the regional structure of the campus, maintain the integrity of the space. At the same time, according to different buildings and areas, bright colors are applied to create a youthful atmosphere and energy for students."},
+
+  works_content_cn: {
+    cover_title: "贝塞斯国际学校，惠州校区",
+    tag_list: ["学校"],
+    title: "贝塞斯国际学校",
+    location:"惠州校区",
+    client: "China Resources",
+    year: "2018",
+    intro: "别树一帜的国际校园风格\n\n贝赛思是美国学术声誉极高的体系学校，由世界级的经济学者Dr. Michael Block 和Olga Block女士创立于1998年。华润小径湾贝赛思国际学校坐落在广东省华润小径湾半山之间，由爱圣国际教育集团有限公司携手华润集团斥资约6亿元联合打造。华润小径湾贝赛思国际学校占地面积68744平方米，总建筑面积43356平方米，容积率仅0.59，建筑覆盖率仅22%。整个校园依山而建，错落有致，绿地面积高达65%，是一座不可多得的生态山海校园。\n" +
+      "\n" +
+      "以校园典型为灵感源，设计充分考虑到使用者需求，研究人群的身高体征、视力可见度、文字水平等，以及行为心理特点等生理需求纳入到该系统的服务范畴中，配合环境和文化学习氛围综合应用在具体的设计当中。在复杂的环境里有效的发挥指示方向和警示作用，使受众人群准确地了解校园的区域构造，保持空间的整体性同时，针对不同的建筑和区域，应用明亮的色彩为学生营造青春的氛围和能量。"
+  },
+
+  works_content_tc: {
+    cover_title: "貝塞斯國際學校，惠州校區",
+    tag_list: ["學校"],
+    title: "貝塞斯國際學校",
+    location:"惠州校區",
+    client: "China Resources",
+    year: "2018",
+    intro: "別樹一幟的國際校園風格\n\n貝賽思是美國學術聲譽極高的體系學校，由世界級的經濟學者Dr. Michael Block 和Olga Block女士創立於1998年。華潤小徑灣貝賽思國際學校坐落在廣東省華潤小徑灣半山之間，由愛聖國際教育集團有限公司攜手華潤集團斥資約6億元聯合打造。華潤小徑灣貝賽思國際學校佔地面積68744平方米，總建築面積43356平方米，容積率僅0.59，建築覆蓋率僅22%。整個校園依山而建，錯落有致，綠地面積高達65%，是一座不可多得的生態山海校園。\n" +
+      "\n" +
+      "以校園典型為靈感源，設計充分考慮到使用者需求，研究人群的身高體徵、視力可見度、文字水平等，以及行為心理特點等生理需求納入到該系統的服務範疇中，配合環境和文化學習氛圍綜合應用在具體的設計當中。在復雜的環境裡有效的發揮指示方向和警示作用，使受眾人群準確地了解校園的區域構造，保持空間的整體性同時，針對不同的建築和區域，應用明亮的色彩為學生營造青春的氛圍和能量。"
+  },
+
+  img_list: [
+    [{
+      proportion: 1,
+      img: "1.png",
+      ratio: 1.510
+    }],
+    [{
+      proportion: 1,
+      img: "2.png",
+      ratio: 2.474
+    }],
+    [{
+      proportion: 1,
+      img: "3.png",
+      ratio: 1.239
+    }, {
+      proportion: 1,
+      img: "4.png",
+      ratio: 1.239
+    }]
+  ]
+}
